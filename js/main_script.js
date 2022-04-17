9b59cd6cad36431daad8dcc46d35640e231f203c
Rafael Roslan <rafaelroslan@gmail.com>
2022-04-17 20:43:35 -0300

    renomeação e comentário

--- js/main_script.js
+++ js/main_script.js
@@ -1,5 +1,5 @@
 //Script de rotas
-//importações das index.js com o contuedos das paginas - no import definimos um nome para o elemento que estamos importando.
+//Importações das index.js com o conteúdos das paginas - no import definimos um nome para o elemento que estamos importando.
 import antagonistas from "../pages/antagonistas/index.js";
 import arcos from "../pages/arcos/index.js";
 import contato from "../pages/contato/index.js";
@@ -14,15 +14,19 @@ import personagens from "../pages/personagens/index.js";
 import quemSomos from "../pages/quem_somos/index.js";
 import xMen  from "../pages/x_men/index.js";
 
-
+//Aqui criamos a constante main que recebe um elemento buscado no documento pelo query selector. Nesse caso o elemento <main> com a id="root".
 const main = document.querySelector("#root");
 
+//Aqui criamos a constante init que recebe uma arrow-function.
 const init = ()=>{
+    //Dentro dessa arrow-function usamos o window.addEventListener para "ouvir" algum evento.
+    //Aqui o window.addEventListener está "ouvindo" qualquer mudança na hashtag da url através do hashchange.
     window.addEventListener("hashchange",()=>{
+        //Aqui limpamos qualquer coisa que esteja dentro da tag <main> ao definir que seu conteúdo é "".
         main.innerHTML = "";
+        //Partir daqui usamos um Switch-Case para um teste logíco, o resultado executa a adição do conteudo na tag <main>.
         switch (window.location.hash) {
             case " ":
-                
                 main.appendChild(home());
                 break;
             case "#historia":
@@ -60,7 +64,7 @@ const init = ()=>{
     });
 }
 
-
+//Aqui usamos o window.addEventListener para "ouvir" o evento de "load", ou seja quando a página carregar ele executará a sequencia de código.
 window.addEventListener("load", ()=>{
     main.appendChild(home());
     init();
