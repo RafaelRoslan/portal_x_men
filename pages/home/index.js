d2c224ebbc149f982b5461d60bd047b1fdfa01d3
Anderson Silva <anderson_silva11000@outlook.com>
2022-04-21 11:12:39 -0300

    adicionado Critica Social,home e imagem

--- pages/home/index.js
+++ pages/home/index.js
@@ -8,39 +8,39 @@ export default () => {
 
     //criação do template - é aqui onde colocamos o todo conteúdo que será exibido na pagina.
     const template = `
-    <div>
-    <article>
-    <h1>aaaaaaaaaaaaa</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe natus modi omnis cum quos rem vitae dicta laudantium eligendi perspiciatis, aperiam et sequi expedita? Obcaecati accusantium cum architecto facilis! Distinctio.</p>
+   <div>
+   <article>
+   <h1>X-Men</h1>
+   <p>Percebendo que os mutantes eram o próximo passo na evolução humana, o professor Charles Xavier reuniu adolescentes talentosos para serem sua primeira turma de alunos. Seu objetivo é proteger e educar a próxima geração de homo superior, enquanto
+       persegue um sonho de harmonia.</p>
 </article>
 <aside>
-    <img src="" alt="" width="400" height="200">
+   <img src="imagens/home_xmen.jpeg" alt="" width="400" height="200">
 </aside>
 <section>
-    <img src="" alt="" width="400" height="200">
-    <h2>Titulo</h2>
-    <p>Voluptates, praesentium harum. Commodi eligendi saepe similique pariatur beatae quis iste magnam quidem praesentium tempora. Quam modi beatae pariatur accusantium, adipisci molestias nobis quod voluptas voluptatum dolor, consequuntur sit animi?
-    </p>
+   <img src="" alt="" width="400" height="200">
+   <h2>Titulo</h2>
+   <p>Voluptates, praesentium harum. Commodi eligendi saepe similique pariatur beatae quis iste magnam quidem praesentium tempora. Quam modi beatae pariatur accusantium, adipisci molestias nobis quod voluptas voluptatum dolor, consequuntur sit animi?
+   </p>
 </section>
 <section>
-    <img src="" alt="" width="400" height="200">
-    <h2>Titulo</h2>
-    <p>Voluptates, praesentium harum. Commodi eligendi saepe similique pariatur beatae quis iste magnam quidem praesentium tempora. Quam modi beatae pariatur accusantium, adipisci molestias nobis quod voluptas voluptatum dolor, consequuntur sit animi?
-    </p>
+   <img src="" alt="" width="400" height="200">
+   <h2>Titulo</h2>
+   <p>Voluptates, praesentium harum. Commodi eligendi saepe similique pariatur beatae quis iste magnam quidem praesentium tempora. Quam modi beatae pariatur accusantium, adipisci molestias nobis quod voluptas voluptatum dolor, consequuntur sit animi?
+   </p>
 </section>
 <section>
-    <img src="" alt="" width="400" height="200">
-    <h2>Titulo</h2>
-    <p>Voluptates, praesentium harum. Commodi eligendi saepe similique pariatur beatae quis iste magnam quidem praesentium tempora. Quam modi beatae pariatur accusantium, adipisci molestias nobis quod voluptas voluptatum dolor, consequuntur sit animi?
-    </p>
+   <img src="" alt="" width="400" height="200">
+   <h2>Titulo</h2>
+   <p>Voluptates, praesentium harum. Commodi eligendi saepe similique pariatur beatae quis iste magnam quidem praesentium tempora. Quam modi beatae pariatur accusantium, adipisci molestias nobis quod voluptas voluptatum dolor, consequuntur sit animi?
+   </p>
 </section>
 <section>
-    <img src="" alt="" width="400" height="200">
-    <h2>Titulo</h2>
-    <p>Voluptates, praesentium harum. Commodi eligendi saepe similique pariatur beatae quis iste magnam quidem praesentium tempora. Quam modi beatae pariatur accusantium, adipisci molestias nobis quod voluptas voluptatum dolor, consequuntur sit animi?
-    </p>
+   <img src="" alt="" width="400" height="200">
+   <h2>Titulo</h2>
+   <p>Voluptates, praesentium harum. Commodi eligendi saepe similique pariatur beatae quis iste magnam quidem praesentium tempora. Quam modi beatae pariatur accusantium, adipisci molestias nobis quod voluptas voluptatum dolor, consequuntur sit animi?
+   </p>
 </section>
-
 </div>
     `;
 
